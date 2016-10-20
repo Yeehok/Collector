9bbe5274829c9907b5d8944b26a3800a55c92705
Yeehok <yeehok@gmail.com>
2016-10-20 11:17:04 +0800

    initialize

---
+++ Collector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Collector
+//
+//  Created by Yeehok on 2016/10/20.
+//  Copyright © 2016年 Yeehok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
